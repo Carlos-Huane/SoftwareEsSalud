d06b55ec6a0b0e8311e9d0f576464b5c52338db2
Carlos-Huane <carloshuanesarmiento@gmail.com>
2024-09-01 12:36:07 -0500

    MVC files

---
+++ JavaApplication8/src/MVC/mvc.java
@@ -0,0 +1,9 @@
+package MVC;
+
+public class mvc {
+    public static void main(String args[]){
+        System.out.print("hola");
+        System.out.print("hola");
+        System.out.print("hola");
+    }
+}
